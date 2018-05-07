CarbonROM/android_device_samsung_star2lte

starlte: Add RR support
Change-Id: Ie14fa5e368783e352c273a7913a48970aebdcafe
Signed-off-by: msdx321 <msdx321@gmail.com>

rr.mk
@@ -1,0 +1,9 @@
+# Copyright (C) 2018 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+
+$(call inherit-product, device/samsung/star2lte/full_star2lte.mk)
+
+# Inherit some common RR stuff.
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
+
+PRODUCT_NAME := rr_star2lte

msdx321/android_device_samsung_starlte

rr.mk
@@ -1,0 +1,9 @@
+# Copyright (C) 2018 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+
+$(call inherit-product, device/samsung/starlte/full_starlte.mk)
+
+# Inherit some common RR stuff.
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
+
+PRODUCT_NAME := rr_starlte